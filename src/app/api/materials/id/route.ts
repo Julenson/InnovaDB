51bcd5e328b4ed03ba027f5b34c640296aec6474
Julenson <93591054+Julenson@users.noreply.github.com>
2025-08-04 10:37:34 +0000

    No se pero que edite xfa

--- src/app/api/materials/id/route.ts
+++ src/app/api/materials/id/route.ts
@@ -1,28 +1,28 @@
-// Este archivo maneja: /api/materials/[id]
+// /app/api/materials/[id]/route.ts
 
 import { NextRequest, NextResponse } from 'next/server';
 import { updateMaterial, deleteMaterial } from '@db/managedb';
 
 export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
   try {
-    const { quantity, lastUpdated, updatedBy, description, name, category } = await request.json();
-    const id = parseInt(params.id, 10);
+    const id = Number(params.id);
+    const { quantity, updatedBy, lastUpdated, description, name, category } = await request.json();
 
     if (!id || quantity === undefined) {
       return NextResponse.json({ error: 'ID and quantity are required' }, { status: 400 });
     }
 
-    const updatedMaterial = await updateMaterial({
+    const updated = await updateMaterial({
       id,
-      name,
-      category,
       quantity,
       updatedBy,
       lastUpdated,
       description,
+      name,
+      category,
     });
 
-    return NextResponse.json(updatedMaterial);
+    return NextResponse.json(updated);
   } catch (error) {
     console.error('Error actualizando material:', error);
     return NextResponse.json({ error: 'Error updating material' }, { status: 500 });
@@ -31,12 +31,7 @@ export async function PUT(request: NextRequest, { params }: { params: { id: stri
 
 export async function DELETE(_: NextRequest, { params }: { params: { id: string } }) {
   try {
-    const id = parseInt(params.id, 10);
-
-    if (!id) {
-      return NextResponse.json({ error: 'ID is required' }, { status: 400 });
-    }
-
+    const id = Number(params.id);
     await deleteMaterial(id);
     return NextResponse.json({ message: 'Material deleted' });
   } catch (error) {
